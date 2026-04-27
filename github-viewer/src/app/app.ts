55aabaa277adb21c33b51b0413027a42bfa87a1d
Jesus S Gomez <js.gomezj1@uniandes.edu.co>
2026-04-26 19:46:12 -0500

    agregando nav bar para hacer cambio entre users and repos

--- github-viewer/src/app/app.ts
+++ github-viewer/src/app/app.ts
@@ -2,13 +2,15 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UsuarioListComponent } from './features/usuarios/usuario-list/usuario-list.component';
 import { RepositorioListComponent } from "./features/repositorios/repositorio-list/repositorio-list.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [UsuarioListComponent, RepositorioListComponent],
+  imports: [UsuarioListComponent, RepositorioListComponent,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  activeTab = 'usuarios';
   protected readonly title = signal('github-viewer');
 }
